2ac2dfb5b995ad4d9fccf6d051a6a7680fe7c088
Sung Dong Kim <67620856+Last-remote11@users.noreply.github.com>
2021-01-20 20:02:15 +0900

    데이터베이스 추가

--- server.js
+++ server.js
@@ -4,40 +4,23 @@ const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors')
 const app = express();
 
-app.use(express.json()); 
-// 원래는 body-parser라는 별개의 패키지였으나 express와 통합됨
+const knex = require('knex');
+const { response } = require('express');
 
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'peko',
+      database : 'facerecognize'
+    }
+  });
+
+
+app.use(express.json()); 
 app.use(cors())
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'peko',
-            password: "miko",
-            email: 'peko@gmail.com',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '456',
-            name: 'houshou',
-            password: "marine",
-            email: 'houshou@gmail.com',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'peko@gmail.com'
-        }
-    ]
-}
-// 임시로 맨든 데이터베이스. 서버를 새로고침하면 추가된건 날아감
 
 
 app.get('/', (req, res) => {
@@ -48,98 +31,88 @@ app.get('/', (req, res) => {
 // signin과 register는 post요청(CRUD의 create)
 app.post('/signin', (req, res) => {
 
-    bcrypt.compare("miko", '$2a$10$iy2MZm6GWuOqm3J8kiG9xeb7pP//nvVavylAeb4Od5uJYE3kUSVQq', function(err, res) {
-        console.log('first guess', res)
-    });
-
-    bcrypt.compare("haha", '$2a$10$iy2MZm6GWuOqm3J8kiG9xeb7pP//nvVavylAeb4Od5uJYE3kUSVQq', function(err, res) {
-        console.log('seconde guess', res)
-    })
-
-    let signinSuccess = false
     const { email, password } = req.body
 
-    database.users.forEach((user, i) => {
-        if (email === user.email && password === user.password) {
-            signinSuccess = true
-            res.json(database.users[i])
+    db.select('email', 'hash').from('login') // select에서는 returning을 사용할 필요가 없다??
+        .where('email', '=', email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(password, data[0].hash);
+            if (isValid) {
+                return db.select('*').from('users')
+                .where('email', '=', email)
+                .then(user => {
+                    res.json(user[0])
+                })
+                .catch(err => res.statusMessage(400).json('로그인 실패'))
+            } else {
+            res.status(400).json("로그인 실패")
         }
-    })
-        if (!signinSuccess) {
-            res.status(400).json('로그인 실패')
-        }
-    
+        })
+        .catch(err => res.status(400).json('로그인 실패'))
+})
 
-    // if (req.body.email === database.users[1].email &&
-    //     req.body.password === database.users[1].password ) {
-    //     res.json('sign in success')
-    // } else {
-    //     res.status(400).json('wrong email or password')
-    // }
 
-    // db와 대조하여 맞으면 로그인 아니면 거부?
-});
 
 app.post('/register', (req, res) => {
 
-    const { email, name, password} = req.body
+    const { email, name, hash} = req.body
     
-    database.users.push(
-        {
-            id: '124',
-            name: name,
-            email: email,
-            entries: 0,
-            joined: new Date()
-        }
-    )
-    res.json(database.users[database.users.length - 1])
+    // 두 가지 이상의 작업을 할 경우 트랜잭션 사용
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail=> {
+            return trx('users')
+                .returning('*') // returning을 하면 insert를 수행한 뒤 결과(?)를 반환해줌
+                .insert({
+                    email: loginEmail[0],
+                    name: name,
+                    joined: new Date() 
+                })
+                .then(user => 
+                    res.json(user[0]))
+                .catch(err => 
+                    res.status(400).json('이미 가입된 이메일입니다.'))
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
+
+
 })
     
 
 app.get('/profile/:id', (req, res) => {
-    let found = false;
+
     const { id } = req.params;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
+
+    db.select('*').from('users').where({id}).then(user => {
+        if (user.length) {
+            res.json(user[0])
+        } else {
+            res.status(400).json('그런 유저는 없습니다.')
         }
     })
-    if (!found) {
-        res.status(400).json('그런 유저는 없습니다.')
-    }     
+    .catch(err => {res.status(400).json('error getting user')})
+
 })
+
 
 app.put('/image', (req, res) => {
-    let found = false
+
     const { id } = req.body;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++
-            return res.json(user.entries);
-        }
+    db('users').where('id' ,'=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0])
     })
-    if (!found) {
-        res.status(400).json('그런 유저는 없습니다.')
-    }
+    .catch(err => res.status(400).json("에러발생"))
 })
-
-
-
-// bcrypt.hash(password, null, null, function(err, hash) {
-//     console.log(hash)
-// });
-
-// // Load hash from your password DB.
-// bcrypt.compare("bacon", hash, function(err, res) {
-//     // res == true
-// });
-// bcrypt.compare("veggies", hash, function(err, res) {
-//     // res = false
-// });
-
 
 
 app.listen(3000, () => {
